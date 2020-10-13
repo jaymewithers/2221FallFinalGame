c108ea440177b5d975001fa6154fc9acadf1ab07
Jayme Withers <10859571@my.uvu.edu>
2020-10-12 18:40:11 -0600

    start of shooting

---
+++ Assets/Scripts/Behaviours/ApplyForce.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rbody;
+    public float force = 500f;
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        var forceDirection = new Vector3(force, 0, 0);
+        rbody.AddRelativeForce(forceDirection);
+    }
+}
