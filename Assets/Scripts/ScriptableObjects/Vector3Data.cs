f0213a830480b9dff979a2f3dec01189b404c954
Jayme Withers <10859571@my.uvu.edu>
2020-10-14 10:16:11 -0600

    shooting

--- Assets/Scripts/ScriptableObjects/Vector3Data.cs
+++ Assets/Scripts/ScriptableObjects/Vector3Data.cs
@@ -4,4 +4,32 @@
 public class Vector3Data : ScriptableObject
 {
     public Vector3 value;
+
+    public void SetValueFromVector3(Vector3 obj)
+    {
+        value = obj;
+    }
+
+    public void SetValueFromPosition(Transform obj)
+    {
+        value = obj.position;
+    }
+
+    public void SetValueFromRotation(Transform obj)
+    {
+        value = obj.eulerAngles;
+    }
+
+    public void SetPositionFromValue(Transform obj)
+    {
+        obj.position = value;
+    }
+
+    public void SetFromMousePosition(Camera cam)
+    {
+        if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out var hit, 100))
+        {
+            value = hit.point;
+        }
+    }
 }
