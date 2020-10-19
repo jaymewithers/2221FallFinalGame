f5674eec4234f30e0155586435096bb1d349e651
Jayme Withers <10859571@my.uvu.edu>
2020-10-19 16:57:54 -0600

    working on death and respawn

--- Assets/Scripts/Behaviours/DeathBehaviour.cs
+++ Assets/Scripts/Behaviours/DeathBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class DeathBehaviour : MonoBehaviour
 {
     public GameObject gameObj;
-    public FloatData health;
+    public FloatData health, playerLives;
     public Vector3Data spawnPoint;
 
     private void Update()
@@ -12,15 +12,21 @@ public class DeathBehaviour : MonoBehaviour
         if (!(health.value <= 0)) return;
         gameObj.SetActive(false);
         StartCoroutine(Respawn());
+
+        if (!(playerLives.value <= 0)) return;
+        gameObj.SetActive(false);
+        print("Game Over");
     }
 
     private IEnumerator Respawn()
     {
-        if (gameObj.name != "Player") yield break;
-        yield return new WaitForSeconds(3f);
-        gameObj.transform.position = spawnPoint.value;
-        health.value = 1f;
-        gameObj.SetActive(true);
-        StopCoroutine(Respawn());
+        if (gameObj.name != "Player" && playerLives.value > 0) yield break;
+        {
+            yield return new WaitForSeconds(3f);
+            gameObj.transform.position = spawnPoint.value;
+            health.value = 1f;
+            gameObj.SetActive(true);
+            StopCoroutine(Respawn());
+        }
     }
 }
---
+++ Assets/Scripts/ScriptableObjects/StringListData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class StringListData : ScriptableObject
+{
+    public List<string> stringList;
+
+    private string returnValue;
+
+    private int i;
+
+    public void GetNextString()
+    {
+        returnValue = stringList[i];
+        i = (i + 1) % stringList.Count;
+    }
+
+    public void SetTextUIToValue(Text obj)
+    {
+        obj.text = returnValue;
+    }
+}
