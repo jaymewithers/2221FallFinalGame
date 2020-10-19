477e0dc28396659dfa77cb97d839dc11233a3df2
Jayme Withers <10859571@my.uvu.edu>
2020-10-19 10:35:14 -0600

    death and respawn

---
+++ Assets/Scripts/Behaviours/DeathBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DeathBehaviour : MonoBehaviour
+{
+    public GameObject gameObj;
+    public FloatData health;
+    public Vector3Data spawnPoint;
+
+    private void Update()
+    {
+        if (!(health.value <= 0)) return;
+        gameObj.SetActive(false);
+        StartCoroutine(Respawn());
+    }
+
+    private IEnumerator Respawn()
+    {
+        if (gameObj.name != "Player") yield break;
+        yield return new WaitForSeconds(3f);
+        gameObj.transform.position = spawnPoint.value;
+        health.value = 1f;
+        gameObj.SetActive(true);
+        StopCoroutine(Respawn());
+    }
+}
