a045262674c3b92a245bae3e39fb6d6fb1d23ca8
Jayme Withers <10859571@my.uvu.edu>
2020-11-09 18:31:49 -0700

    Fixed camera

---
+++ Assets/Scripts/Behaviours/PlayerCamFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCamFollow : MonoBehaviour
+{
+    public Transform player;
+    private Vector3 position;
+
+    private void Update()
+    {
+        var xInput = player.position.x;
+        var zInput = player.position.z;
+        position = new Vector3(xInput,4 , zInput);
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/Behaviours/PushPull.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PushPull : MonoBehaviour
+{
+    private Rigidbody rBody;
+    public bool canPickup;
+    public Transform player;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (canPickup && Input.GetKeyDown(KeyCode.Z))
+        {
+            var transformObj = transform;
+            transformObj.parent = player;
+            canPickup = false;
+        }
+
+        if (!Input.GetKeyUp(KeyCode.Z)) return;
+        {
+            var transformObj = transform;
+            transformObj.parent = null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canPickup = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canPickup = false;
+    }
+}
