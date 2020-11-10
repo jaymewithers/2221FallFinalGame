ecb12f0dd8f5dd5c2495a27a1d9e138e289999bf
Jayme Withers <10859571@my.uvu.edu>
2020-11-09 21:03:22 -0700

    push pull / working on knockover

---
+++ Assets/Scripts/Cover.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Cover : MonoBehaviour
+{
+    public bool canUse;
+    public FloatData coverData;
+    public Transform player;
+    public Vector3 offset, restOffset;
+    public UnityEvent disableEvent, enableEvent;
+    
+    private void Start()
+    {
+        coverData.value = 0f;
+        canUse = false;
+    }
+    
+    private void Update()
+    {
+        if (canUse && Input.GetKeyDown(KeyCode.X))
+        {
+            coverData.value = 1f;
+            disableEvent.Invoke();
+            var transform1 = transform;
+            transform1.position = player.position + offset;
+            transform1.parent = player;
+            canUse = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            coverData.value = 0f;
+            var transform1 = transform;
+            transform1.parent = null;
+            transform1.position = player.position + restOffset;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canUse = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enableEvent.Invoke();
+            canUse = false;
+        }
+    }
+}
---
+++ Assets/Scripts/KnockOver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class KnockOver : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            other.transform.Rotate(0, 0, 90);
+        }
+    }
+}
