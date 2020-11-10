a30d35c3e5c4f5039cb5450ba44d8d328f7237c0
Jayme Withers <10859571@my.uvu.edu>
2020-11-10 12:33:29 -0700

    Tip over

--- Assets/Scripts/KnockOver.cs
+++ Assets/Scripts/KnockOver.cs
@@ -1,12 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 public class KnockOver : MonoBehaviour
 {
+    public bool canKnockOver;
+    public float knockedOver;
+    public Transform obj;
+    public UnityEvent spillEvent;
+
+    private void Start()
+    {
+        canKnockOver = false;
+        knockedOver = 1;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        canKnockOver = true;
+        print("Triggered");
+    }
+
+    private void Update()
+    {
+        if (canKnockOver && knockedOver >= 1 && Input.GetKeyDown(KeyCode.C))
         {
-            other.transform.Rotate(0, 0, 90);
+            obj.transform.Rotate(90, 0, 0);
+            spillEvent.Invoke();
+            knockedOver = 0;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canKnockOver = false;
     }
 }
