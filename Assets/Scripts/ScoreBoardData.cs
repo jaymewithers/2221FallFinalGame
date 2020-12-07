4560acf43841ccf0c0457163ee8829f5a22a1b90
Jayme Withers <10859571@my.uvu.edu>
2020-12-06 17:57:07 -0700

    scoreboard work

--- Assets/Scripts/ScoreBoardData.cs
+++ Assets/Scripts/ScoreBoardData.cs
@@ -1,24 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreBoardData : MonoBehaviour
 {
-    private Text textObj;
-    public IntListData intList;
-    public StringListData stringList;
+    //private Text textObj;
 
-    public StringData stringObj;
-    public IntData intObj;
+    //public StringData stringObj;
+    //public IntData intObj;
 
-    private void Start()
+    //public int score1, score2, score3, score4, score5;
+    //public int newScore1, newScore2, newScore3, newScore4, newScore5;
+    //public List<IntData> scoreList;
+    //public IntData score1, score2, score3, score4, score5;
+
+    // private void Start()
+    // {
+    //     textObj = GetComponent<Text>();
+    // }
+
+    public void ListToText(List<int> list)
     {
-        textObj = GetComponent<Text>();
+        var result = "";
+        foreach (var listValue in list)
+        {
+            result += listValue.ToString() + "\n";
+        }
     }
 
-    public void HandleScore()
+    public void SortList(List<int> intList)
     {
-        textObj.text = stringObj.value + intObj;
+        intList.Sort();
     }
+
+    // public void AddToScores(IntData value)
+    // {
+    //     scoreList.Add(value);
+    //     if (scoreList.Capacity > 1)
+    //     { 
+    //         scoreList.Sort();
+    //     }
+    //     //scoreList.Capacity = 5;
+    // }
+
+    // public void UpdateScores()
+    // {
+    //     score1 = scoreList[1];
+    //     if (scoreList.Capacity == 2)
+    //     {
+    //         score2 = scoreList[2];
+    //     }
+    //     if (scoreList.Capacity == 3)
+    //     {
+    //         score2 = scoreList[3];
+    //     }
+    //     if (scoreList.Capacity == 4)
+    //     {
+    //         score2 = scoreList[4];
+    //     }
+    //     if (scoreList.Capacity == 5)
+    //     {
+    //         score2 = scoreList[5];
+    //     }
+    // }
+
+    // public void HandleScore()
+    // {
+    //     textObj.text = stringObj.value + intObj;
+    // }
+
+    // public void SortScores()
+    // {
+    //     scoreList.Sort();
+    // }
 }
